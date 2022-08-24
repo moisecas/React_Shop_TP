5893c018224ca68b0a6b872c2a4dfd0ff5e734be
Your Name <moisesjuniorcas20@gmail.com>
2022-08-23 21:46:15 -0500

    route de componentes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Signup from './Components/Signup';
 import Login from './Components/Login';
 import Navbar from './Components/Navbar';
 import NotFound from './Components/NotFound';
+import Cart from './Components/Cart';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/navbar" element={<Navbar />} /> 
+        <Route path="/cart" element={<Cart />} /> 
         <Route path="*" element={<NotFound />} /> {/* * means any other path */}
       </Routes>
 
