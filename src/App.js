e1ab34530099a1e9a58cb68725b848655f0ff621
Your Name <moisesjuniorcas20@gmail.com>
2022-08-26 19:11:10 -0500

    agregando componentes nuevos

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Login from './Components/Login';
 import Navbar from './Components/Navbar';
 import NotFound from './Components/NotFound';
 import Cart from './Components/Cart';
+import Profile from './Components/Profile';
 
 
 function App() {
@@ -16,8 +17,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/navbar" element={<Navbar />} /> 
+        {/* <Route path="/navbar" element={<Navbar />} />  */} 
         <Route path="/cart" element={<Cart />} /> 
+        <Route path="/userprofile" element={<Profile />} />
+
         <Route path="*" element={<NotFound />} /> {/* * means any other path */}
       </Routes>
 
