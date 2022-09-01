c32c24fb972764af92610860e4e68084abd43e56
Your Name <moisesjuniorcas20@gmail.com>
2022-09-01 16:57:58 -0500

    cambiando Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react' 
-import { Link } from 'react-router-dom'
+import { Link, Navigate, useNavigate } from 'react-router-dom'
 import '../CSS/Navbar.css'
 import cart from '../Components/assets/cart.png'
 import profile from '../Components/assets/profile.png'
@@ -32,29 +32,62 @@ const Navbar = () => {
     return user;
 }
  const loggedUser = GetCurrentUser();
+
+ const navigate = useNavigate();
+
+
+ const handleLogout = () => {
+    auth.signOut().then(()=>{
+      navigate('/login'); 
+    })
+ } 
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link to="/"><button>Home</button> </Link>
-        <Link to="/signup"><button>Register</button> </Link>
-        <Link to="/login"><button>Login</button> </Link>
-        <Link to="/products"><button>Products</button> </Link>
+    <div>
+      <div className="navbar">
+        <div className="LeftContainer">
+        </div>
+        <div>
+        {!loggedUser && <nav>
+          <Link to="/"><button>Home</button></Link>
+          <Link to="/signup"><button>Register</button></Link>
+          <Link to="/login"><button>Login</button></Link> 
 
-        <Link to="/cart">
+          <div className='cart-btn'>
+            <img src={cart} alt="cart" />
+            <span className='cart-count'>0</span>
+          </div>
+          <Link to="/profile">
+            <img src={profile} alt="profile"  className='profile-icon'/>
+          </Link>
+          </nav>}
+
+          {loggedUser  && <nav>
+            <Link to="/"><button>Home</button></Link>
+            <Link to="/products"><button>Products</button></Link>
+
             <div className='cart-btn'>
-                <img src={cart} alt='cart' />
-                <span className='cart-icon-css' >0</span> 
-            
+              <img src={cart} alt="cart" />
+              <span className='cart-count'>{loggedUser[0].cart}</span> 
             </div>
-        </Link>
+            <Link to="/profile">
+              <img src={profile} alt="profile" />
+            </Link>
+            <button className='logout-btn' onClick={handleLogout}>Logout</button>  
+            </nav>
+          
+          }
+        </div>
+      </div>
+      <div className='product-types'>
+      
+         <a href='/product-type/skinps4'><button>Skin PS4</button> </a>
+          <a href='/product-type/skinxbox'><button>Skin Xbox</button> </a>
+          <a href='/product-type/skinps3'><button>Skin PS3</button> </a>
+          <a href='/product-type/skinnintendo'><button>Skin Nintendo</button> </a>
 
-        
-        <Link to="userprofile">
-            <img src={profile} className='profile-icon' />
-        </Link>
-        <p>{loggedUser ? loggedUser[0].email : "No user"}</p>
-        {loggedUser ? <button onClick={()=>{auth.signOut()}}>Logout</button> : null} 
-        
-    </nav>
+      </div>
+    </div>
   )
 }
 
